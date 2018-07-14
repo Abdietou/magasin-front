13f70c5511ffa31b45499d7d216a47b8f459c58c
Abdietou <a.doucoure@ymail.com>
2018-07-15 01:43:12 +0200

    first commit

---
+++ src/boutiquelivre-back/src/main/java/com/boutiquelivre/BoutiquelivreBackApplication.java
@@ -0,0 +1,12 @@
+package com.boutiquelivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiquelivreBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiquelivreBackApplication.class, args);
+	}
+}
